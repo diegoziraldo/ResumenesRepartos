869abba7c021c7db9bed844ed1761308a923184a
diegoziraldo <ziraldodiego@gmail.com>
2023-11-20 13:45:53 -0300

    201123

--- editar.php
+++ editar.php
@@ -1,7 +1,4 @@
 <?php
-if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['id'])) {
-    $id = $_POST['id'];
-    // Aquí puedes realizar las acciones necesarias para editar el registro con el ID proporcionado
-    echo "Editar el registro con ID: $id";
+    if($_POST['editar']) {
+        echo 'editar';
 }
-?>
